409ba3cdf8755572c78fcf687fce463a42ab77ef
nanadotam <nanaamoako202@gmail.com>
2024-07-26 17:03:47 +0000

    Changes

--- Code/Tree.java
+++ Code/Tree.java
@@ -1,33 +1,33 @@
 import java.util.ArrayList;
 
 class TreeNode<T> {
-  private final T root;
-  private TreeNode<T> parent;
-  private final ArrayList<TreeNode<T>> children;
+    private final T root;
+    private TreeNode<T> parent;
+    private final ArrayList<TreeNode<T>> children;
 
-  public TreeNode(T root) {
-    this.root = root;
-    children = new ArrayList<>();
-  }
+    public TreeNode(T root) {
+        this.root = root;
+        children = new ArrayList<>();
+    }
 
-  public TreeNode<T> addChild(T child) {
-    TreeNode<T> childNode = new TreeNode<T>(child);
-    childNode.parent = this;
-    this.children.add(childNode);
-    return childNode;
-  }
+    public TreeNode<T> addChild(T child) {
+        TreeNode<T> childNode = new TreeNode<T>(child);
+        childNode.parent = this;
+        this.children.add(childNode);
+        return childNode;
+    }
 
-  public T getRoot() {
-    return root;
-  }
+    public T getRoot() {
+        return root;
+    }
 
-  public boolean isRoot() {
-    return parent == null;
-  }
+    public boolean isRoot() {
+        return parent == null;
+    }
 
-  public boolean isLeaf() {
-    return children.size() == 0;
-  }
+    public boolean isLeaf() {
+        return children.size() == 0;
+    }
 
   public int getLevel() {
     if (this.isRoot())
