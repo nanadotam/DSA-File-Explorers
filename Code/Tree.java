72f460593cd890ec50a83d78c69841eb34718757
leyrieee <123450908+leyrieee@users.noreply.github.com>
2024-07-26 16:59:41 +0000

    Update Tree.java

--- Code/Tree.java
+++ Code/Tree.java
@@ -1,35 +1,43 @@
-import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
 
-// java code for above approach
+class TreeNode<T> {
+  private final T root;
+  private TreeNode<T> parent;
+  private final ArrayList<TreeNode<T>> children;
 
-public class TreeNode<T> {
-    private final T root;
-    private T parent;
-    private List<TreeNode> children;
-
-    public TreeNode(T data) {
-        this.data = data;
-        this.children = new ArrayList<>();
-    }
-
-    public TreeNode<T> addChild(T child) {
-        TreeNode<T> childNode = new TreeNode<T>(child);
-        childNode.parent = this;
-        this.children.add(childNode);
-        return childNode;
+  public TreeNode(T root) {
+    this.root = root;
+    children = new ArrayList<>();
   }
 
-    public TreeNode<T> getRoot() { 
-        return root;
-    }
-<<<<<<< HEAD
+  public TreeNode<T> addChild(T child) {
+    TreeNode<T> childNode = new TreeNode<T>(child);
+    childNode.parent = this;
+    this.children.add(childNode);
+    return childNode;
+  }
 
-    
-    
+  public T getRoot() {
+    return root;
+  }
+
+  public boolean isRoot() {
+    return parent == null;
+  }
+
+  public boolean isLeaf() {
+    return children.size() == 0;
+  }
+
+  public int getLevel() {
+    if (this.isRoot())
+      return 0;
+    else
+      return parent.getLevel() + 1;
+  }
+
+  @Override
+  public String toString() {
+    return root != null ? root.toString() : "null";
+  }
 }
-
-
-=======
-}
->>>>>>> fb88ff6 (Changes made)
